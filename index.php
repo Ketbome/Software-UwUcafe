868c464428283034bc73aa5c9ae2f1a54b4626aa
Waluigi15243 <81125222+Waluigi15243@users.noreply.github.com>
2022-06-27 16:22:30 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php 
+	session_start();
+
+	if(isset($_SESSION['nombre'])){
+		if(!isset($_SESSION['type'])){
+			echo("Error logueo");
+		}else if($_SESSION['type'] == 'Admin'){
+			header("location: ../admin/inicio.php");
+		}else if($_SESSION['type'] == 'Chef'){
+			header("location: ../chef/chef.php");
+		}else if($_SESSION['type'] == 'Mesero'){
+			header("location: ../mesero/mesero.php");
+		}else{
+			echo("Error logueo");
+		}
+	}
+?>
+			<header>
+				<link rel="stylesheet" href="main.css">
+			</header>
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+			<body>
+				<nav class="navegacion">
+					<a href="#">
+						<img src="Img/cafe.png" alt="" class="img_left">
+					</a>
+					<ul class="menu">
+
+						<li class="first-item">
+							<a href="login.php">
+								<img src="img/LETRAS.png" alt="" class="imagen">
+								<span class="text-item">Iniciar sesion</span>
+								<span class="down-item"></span>
+							</a>
+						</li>
+					</ul>
+					<a href="#">
+						<img src="img/pan.png" alt="" class="img_right">
+					</a>
+				</nav>
+				<div class="index">
+					<img src="img/LOGO2.png" alt="" class="img_index">
+				</div>
+			</body>
