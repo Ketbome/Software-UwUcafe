ebf7a85195dbe0ec043ac265feae7c6db5485404
Waluigi15243 <81125222+Waluigi15243@users.noreply.github.com>
2022-06-27 16:20:48 -0400

    Add files via upload

--- BD/loguer.php
+++ BD/loguer.php
@@ -13,10 +13,16 @@ $type = mysqli_fetch_array($consulta);
 if(!isset($type[0])){
     echo("Error logueo");
 }else if($type[0] == 'Admin'){
+	$_SESSION['nombre'] = $username;
+	$_SESSION['type'] = $type[0];
     header("location: ../admin/inicio.php");
 }else if($type[0] == 'Chef'){
+	$_SESSION['nombre'] = $username;
+	$_SESSION['type'] = $type[0];
     header("location: ../chef/chef.php");
 }else if($type[0] == 'Mesero'){
+	$_SESSION['nombre'] = $username;
+	$_SESSION['type'] = $type[0];
     header("location: ../mesero/mesero.php");
 }else{
     echo("Error logueo");
