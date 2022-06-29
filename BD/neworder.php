245ec073ac22498da2cf1886ad5ee80cbad67f60
Waluigi15243 <81125222+Waluigi15243@users.noreply.github.com>
2022-06-28 20:00:49 -0400

    Add files via upload

---
+++ BD/neworder.php
@@ -0,0 +1,51 @@
+<?php
+require 'conexion.php';
+session_start();
+
+$type = $_POST['type'];
+$producto1 = $_POST['producto1'];
+$cantidad1 = $_POST['cantidad1'];
+$producto2 = $_POST['producto2'];
+$cantidad2 = $_POST['cantidad2'];
+$mesa = $_POST['mesa'];
+$orderstatus = $_POST['orderstatus'];
+
+$sql = "SELECT cantidad FROM productos WHERE prod_name = '$producto1';";
+$query1 = mysqli_query($conexion,$sql);
+$row = mysqli_fetch_array($query1);
+if($producto1 == 'none'){
+	$producto1 = 'none';
+}
+elseif($row['cantidad'] - $cantidad1 < 0){
+    header("location: ../mesero/mesero.php");
+}
+$sql = "SELECT cantidad FROM productos WHERE prod_name = '$producto2';";
+$query2 = mysqli_query($conexion,$sql);
+$row = mysqli_fetch_array($query2);
+if($producto2 == 'none'){
+	$producto2 = 'none';
+}
+elseif($row['cantidad'] - $cantidad2 < 0){
+    header("location: ../mesero/mesero.php");
+}
+
+$sql = "INSERT INTO pedidos(mesa, orderstatus) VALUES ('$mesa', '$orderstatus');";
+
+if($producto1 != 'none'){
+    $sql2 = "INSERT INTO pedidos_productos(mesa, producto, cantidad) VALUES ('$mesa', '$producto1', $cantidad1)";
+    $conexion->query($sql2);
+}
+if($producto2 != 'none'){
+    $sql3 = "INSERT INTO pedidos_productos(mesa, producto, cantidad) VALUES ('$mesa', '$producto2', $cantidad2)";
+    $conexion->query($sql3);
+}
+
+if ($conexion->query($sql) === TRUE) {
+    if($type == 'Mesero'){
+        header("location: ../mesero/mesero.php");
+    }else{
+        header("location: ../admin/inicio.php");
+    }
+}
+$conexion->close();
+?>
